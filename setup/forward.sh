22e40e436dca1b86c6908a62b61227adfba8fa91
Lorenzo Fontana <lo@linux.com>
2019-07-17 17:42:16 +0000

    new: grafana setup
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- setup/forward.sh
+++ setup/forward.sh
@@ -1,1 +1,2 @@
-kubectl port-forward --address 0.0.0.0 svc/prometheus 9090:9090
+kubectl port-forward --address 0.0.0.0 svc/prometheus 9090:9090 &
+kubectl port-forward --address 0.0.0.0 svc/grafana 3000:3000
